c89f0b64f8df785a6356b8c596c408b44dc90901
Vincent-Luu <vincentluu27@gmail.com>
2025-05-16 21:49:47 +0800

    renamed:    lib/android_app_launcher.dart -> lib/MainPage/Action/android_app_launcher.dart 	new file:   lib/MainPage/cards.dart 	modified:   lib/main.dart

---
+++ lib/MainPage/cards.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'Action/android_app_launcher.dart';
+
+class EachCard extends StatelessWidget {
+  const EachCard(
+    this.gameName,
+    this.gameDescribe,
+    this.imageUrl,
+    this.packageName,
+    this.packageName2,
+    this.isVisible,
+    this.gameUrl, {
+    super.key,
+  });
+
+  final String gameName;
+  final String imageUrl;
+  final String gameDescribe;
+  final String packageName;
+  final String packageName2;
+  final bool isVisible;
+  final String gameUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Card(
+          child: Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(5),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Image.asset(
+                          imageUrl,
+                          width: 80,
+                          height: 80,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      SizedBox(width: 12),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              gameName,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                            Text(
+                              gameDescribe,
+                              style: TextStyle(
+                                color: Colors.grey[600],
+                                fontSize: 14,
+                              ),
+                              softWrap: true,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: TextButton(
+                        onPressed: () => _launchGame(context),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('国  服 '),
+                            Icon(Icons.play_arrow_rounded),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Row(
+                        children: [
+                          Expanded(flex: 1, child: SizedBox()),
+
+                          Expanded(
+                            flex: 2,
+                            child: TextButton(
+                              onPressed: () => _launchGame2(context),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text('国际服 '),
+                                  Icon(Icons.play_arrow_rounded),
+                                ],
+                              ),
+                            ),
+                          ),
+
+                          Expanded(flex: 1, child: SizedBox()),
+                        ],
+                      ),
+                    ),
+                    Visibility(
+                      visible: isVisible,
+                      child: Expanded(
+                        flex: 1,
+                        child: TextButton(
+                          onPressed: () => _launchGameByUrl(context),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('云游戏 '),
+                              Icon(Icons.play_arrow_rounded),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        SizedBox(height: 7),
+      ],
+    );
+  }
+
+  Future<void> _launchGame(BuildContext context) async {
+    try {
+      await AndroidAppLauncher.launchByPackageName(packageName);
+    } catch (e) {
+      ScaffoldMessenger.of(
+        // ignore: use_build_context_synchronously
+        context,
+      ).showSnackBar(SnackBar(content: Text('启动失败: $e')));
+    }
+  }
+
+  Future<void> _launchGame2(BuildContext context) async {
+    try {
+      await AndroidAppLauncher.launchByPackageName(packageName2);
+    } catch (e) {
+      ScaffoldMessenger.of(
+        // ignore: use_build_context_synchronously
+        context,
+      ).showSnackBar(SnackBar(content: Text('启动失败: $e')));
+    }
+  }
+
+  Future<void> _launchGameByUrl(BuildContext context) async {
+    try {
+      await AndroidAppLauncher.launchByUrl(gameUrl);
+    } catch (e) {
+      {
+        ScaffoldMessenger.of(
+          // ignore: use_build_context_synchronously
+          context,
+        ).showSnackBar(SnackBar(content: Text('启动失败: $e')));
+      }
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
-import 'android_app_launcher.dart';
+import 'MainPage/cards.dart';
 
 // 默认颜色（如果设备不支持动态颜色）
 final _defaultLightScheme = ColorScheme.fromSeed(
@@ -39,12 +39,40 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _seletedIndex = 0;
+  final List<Widget> _pages = [
+    MainPageMeow(),
+    SizedBox()
+  ];
+
+  void _onItemTapped(int index){
+    setState(() {
+      _seletedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _seletedIndex,
+        onDestinationSelected: _onItemTapped,
+
+
+        destinations:const<NavigationDestination> [
+          NavigationDestination(icon: Icon(Icons.ads_click), label: '老爸'),
+
+          NavigationDestination(icon: Icon(Icons.add_ic_call_outlined), label: '铸币')
+        ],
+      ),
       appBar: AppBar(
         title: Row(
           children: [
@@ -53,7 +81,17 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-      body: Padding(
+      body: _pages[_seletedIndex],
+    );
+  }
+}
+
+class MainPageMeow extends StatelessWidget {
+  const MainPageMeow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
         padding: EdgeInsets.only(left: 13, right: 13),
         child: ListView(
           children: [
@@ -66,7 +104,6 @@ class HomePage extends StatelessWidget {
               false,
               'null',
             ),
-            SizedBox(height: 7),
             EachCard(
               '原神',
               '开放世界冒险，探索七国，集结角色，对抗强敌，解开提瓦特大陆之谜。',
@@ -76,7 +113,6 @@ class HomePage extends StatelessWidget {
               true,
               'https://ys.mihoyo.com/cloud/',
             ),
-            SizedBox(height: 7),
             EachCard(
               '崩坏：星穹铁道',
               '米哈游科幻冒险RPG，穿梭银河列车，邂逅星神与命途，开启星际奇幻之旅！',
@@ -86,7 +122,6 @@ class HomePage extends StatelessWidget {
               true,
               'https://sr.mihoyo.com/cloud',
             ),
-            SizedBox(height: 7),
             EachCard(
               '绝区零',
               '米哈游全新都市奇幻ARPG，潮酷战斗与赛博朋克风交织，探索未知「空洞」危机！',
@@ -98,160 +133,6 @@ class HomePage extends StatelessWidget {
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class EachCard extends StatelessWidget {
-  const EachCard(
-    this.gameName,
-    this.gameDescribe,
-    this.imageUrl,
-    this.packageName,
-    this.packageName2,
-    this.isVisible,
-    this.gameUrl, {
-    super.key,
-  });
-
-  final String gameName;
-  final String imageUrl;
-  final String gameDescribe;
-  final String packageName;
-  final String packageName2;
-  final bool isVisible;
-  final String gameUrl;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        width: double.infinity,
-        padding: EdgeInsets.all(13),
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(5),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(12),
-                    child: Image.asset(
-                      imageUrl,
-                      width: 80,
-                      height: 80,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  SizedBox(width: 12),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          gameName,
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                          gameDescribe,
-                          style: TextStyle(
-                            color: Colors.grey[600],
-                            fontSize: 14,
-                          ),
-                          softWrap: true,
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: TextButton(
-                    onPressed: () => _launchGame(context),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [Text('国  服 '), Icon(Icons.play_arrow_rounded)],
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: TextButton(
-                    onPressed: () => _launchGame2(context),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [Text('国际服 '), Icon(Icons.play_arrow_rounded)],
-                    ),
-                  ),
-                ),
-                Visibility(
-                  visible: isVisible,
-                  child: Expanded(
-                    flex: 1,
-                    child: TextButton(
-                      onPressed: () => _launchGameByUrl(context),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('云游戏 '),
-                          Icon(Icons.play_arrow_rounded),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Future<void> _launchGame(BuildContext context) async {
-    try {
-      await AndroidAppLauncher.launchByPackageName(packageName);
-    } catch (e) {
-      ScaffoldMessenger.of(
-        // ignore: use_build_context_synchronously
-        context,
-      ).showSnackBar(SnackBar(content: Text('启动失败: $e')));
-    }
-  }
-
-  Future<void> _launchGame2(BuildContext context) async {
-    try {
-      await AndroidAppLauncher.launchByPackageName(packageName2);
-    } catch (e) {
-      ScaffoldMessenger.of(
-        // ignore: use_build_context_synchronously
-        context,
-      ).showSnackBar(SnackBar(content: Text('启动失败: $e')));
-    }
-  }
-
-  Future<void> _launchGameByUrl(BuildContext context) async {
-    try {
-      await AndroidAppLauncher.launchByUrl(gameUrl);
-    } catch (e) {
-      {
-        ScaffoldMessenger.of(
-          // ignore: use_build_context_synchronously
-          context,
-        ).showSnackBar(SnackBar(content: Text('启动失败: $e')));
-      }
-    }
+      );
   }
 }
